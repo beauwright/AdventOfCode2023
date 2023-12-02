a9b6d47d122e60d0d4fb1f6d74789a19489f8544
beauwright <93743705+beauwright@users.noreply.github.com>
2023-12-02 13:41:42 -0700

    Remove redundant code

--- Day2/src/main/kotlin/Main.kt
+++ Day2/src/main/kotlin/Main.kt
@@ -1,18 +1,19 @@
 fun main(args: Array<String>) {
     if (args.isNotEmpty()) {
         val filePath = args[0]
-        solvePartOne(filePath)
-        solvePartTwo(filePath)
+        val scanner = DiceScanner(filePath)
+        scanner.scanLines()
+        val games = scanner.games
+        solvePartOne(games)
+        solvePartTwo(games)
     } else {
         println("Please provide the file path as a command line argument.")
     }
 }
 
-fun solvePartOne(filePath: String) {
-    val scanner = DiceScanner(filePath)
-    scanner.scanLines()
+fun solvePartOne(games: MutableCollection<Game>) {
     var sumOfGameIDs = 0
-    for (game in scanner.games) {
+    for (game in games) {
         if (game.redDice.number <= 12 && game.greenDice.number <= 13 && game.blueDice.number <= 14) {
             sumOfGameIDs += game.gameNumber
         }
@@ -20,11 +21,9 @@ fun solvePartOne(filePath: String) {
     println(sumOfGameIDs)
 }
 
-fun solvePartTwo(filePath: String) {
-    val scanner = DiceScanner(filePath)
-    scanner.scanLines()
+fun solvePartTwo(games: MutableCollection<Game>) {
     var sumOfMinPowers = 0
-    for (game in scanner.games) {
+    for (game in games) {
         val minPower : Int = game.redDice.number * game.greenDice.number * game.blueDice.number
         sumOfMinPowers += minPower
     }
